b773fbe8f01ec36550d2114bfe9e85bf3cfb8165
vrajbhut <vrajbhut@gmail.com>
2022-07-25 11:24:54 -0500

    Initial commit

---
+++ src/test/java/com/test/eurekaDiscovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.eurekaDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
